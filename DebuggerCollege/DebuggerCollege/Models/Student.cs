ff0bce057bae078de9c1c7c918489ce61361f44e
p2630965 <p2630965@GWHXX473WIN011W>
2022-04-26 14:26:09 +0100

    created background for the front end layer fixed minor errors website should be running without any errors

--- DebuggerCollege/DebuggerCollege/Models/Student.cs
+++ DebuggerCollege/DebuggerCollege/Models/Student.cs
@@ -7,5 +7,9 @@ namespace DebuggerCollege.Models
 {
     public class Student
     {
+        public virtual string FullName { get; set; }
+        public virtual int PhoneNo { get; set; }
+        public virtual string Email { get; set; }
+        public virtual Courses courses { get; set; }
     }
 }
